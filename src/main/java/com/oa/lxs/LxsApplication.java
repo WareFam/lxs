17af7fc72a2fb278bc1cb4416c5ee764566253d9
New Man <3400585239@qq.com>
2022-08-30 15:03:25 +0800

    FirstTime CreateProject

---
+++ src/main/java/com/oa/lxs/LxsApplication.java
@@ -0,0 +1,13 @@
+package com.oa.lxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxsApplication.class, args);
+    }
+
+}
